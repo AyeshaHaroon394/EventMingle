9f836c91b6293ca38b4ae16d85b00261760b32fb
AyeshaHaroon394 <ayeshaharoon394@gmail.com>
2025-05-04 04:35:29 +0500

    firebase synced

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
+    // Your existing Android plugin alias
     alias(libs.plugins.android.application) apply false
+
+    // Firebase Google Services plugin (added here, but not applied yet)
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
